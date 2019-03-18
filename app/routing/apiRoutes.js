edd2490659e7b9a3ec418ad7e90f0d00a2fb8e7c
Felix Villalobos <felix.evl@gmail.com>
2019-03-18 10:51:16 -0500

    Change in paths

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,16 +1,13 @@
 
 var friend= require('../data/friends.js');
 
-
 module.exports = function(app) {
-
 	app.get("/api/friends", function(req,res) {
 		res.json(friend);
 	});
 
   app.post("/api/friends", function (req, res){
-      friend.push(req.body);
-      res.json(friend);
+    friend.push(req.body);
+    res.json(friend);
   });
-
 };
